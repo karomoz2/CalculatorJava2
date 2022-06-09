15d5ec95a0efc606dc2dffb218c219d4a5ee9096
karomoz2 <karomoz2@yandex.ru>
2022-06-10 01:11:33 +0300

    0.03
    Написал перевод в обратную польскую нотацию, создал класс Main, чтобы проверить как работает перевод, пока всё удачно

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,8 +4,35 @@ public class Calculator {
 
     public static String ExptoRPolish(String expr){
        String cur="";
-       Stack<Character> stack = new Stack;
-        return null;
+       Stack<Character> stack = new Stack<>();
+       int prior; //Приоритет
+
+       for(int i=0; i<expr.length(); i++)
+       {
+           prior=GetPriority(expr.charAt(i));
+
+           if (prior==0) cur+=expr.charAt(i);
+           if (prior ==1) stack.push(expr.charAt(i));
+           if (prior > 1)
+           {
+               cur+=' '; //Надо чтобы у нас строка не получалась слитной
+               while(!stack.empty())
+               {
+                   if (GetPriority(stack.peek())>=prior) cur+=stack.pop(); //Если верхнего приоритет >= текущего
+                   else break;
+
+               }
+               stack.push(expr.charAt(i));
+           }
+           if (prior==-1){
+               cur+=' ';
+               while(GetPriority(stack.peek())!=1)cur+=stack.pop();
+               stack.pop();
+           }
+       }
+
+       while(!stack.empty())cur+=stack.pop();
+        return cur;
     };
 
     public static double RPolishToResult(String rpn)
@@ -13,7 +40,7 @@ public class Calculator {
         return 0;
     }
 
-    private int GetPriority(char token)
+    private static int GetPriority(char token)
     {
         if(token=='*'|| token=='/')          return 3;
         else if (token =='+' || token =='-') return 2;
@@ -22,10 +49,9 @@ public class Calculator {
         else return 0;
     }
 
-    }
-
-
-
-
-
 }
+
+
+
+
+
