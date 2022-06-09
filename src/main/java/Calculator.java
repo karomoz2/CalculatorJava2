d61edef96dcaa49d53d3a2d05c1aff337c7ded43
karomoz2 <karomoz2@yandex.ru>
2022-06-10 00:33:16 +0300

    0.01
    Продумал логику, начал реализацию

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,26 @@
-import java.io.InputStream;
-import java.util.Scanner;
-import java.util.Vector;
 
 public class Calculator {
 
-   
+    public static String ExptoRPolish(String expr){
+        return null;
+    };
+
+    public static double RPolishToResult(String rpn)
+    {
+        return 0;
+    }
+
+    private int GetPriority(char token)
+    {
+        if(token=='*'|| token=='/') return 3;
+        else if (token =='+') return 2;
+        else if (token == '(') return 1;
+    }
+
+    }
+
+
+
+
+
 }
